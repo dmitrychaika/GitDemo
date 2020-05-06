ee897665d16f9cd0510a732ec0683acc12adf277
Дмитрий Чайка <dmitrychayka@MacBook-Pro-Dmitrij.local>
2020-05-06 17:23:15 +0300

    initial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Дмитрий Чайка on 03.05.2020.
+//  Copyright © 2020 Dmitry Chaika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello world")
+        print("bye world")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

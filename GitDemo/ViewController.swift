45f66c9a43dd5714c03226ece2ddd371f5c917f2
Dmitry_Chaika <dmitrychaika@yahoo.com>
2020-05-08 13:49:50 +0300

    Update ViewController.swift

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("hello baby")
+        print("bye baby")
         // Do any additional setup after loading the view.
     }
 
